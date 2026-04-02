1a9e93ecee2a7259c19af87aa13e52f1125502b9
Pierce Clark <pclark@g.hmc.edu>
2026-04-01 20:12:36 -0700

    fixed signal instantiations, sizes, added comments

--- examples/exercises/lynn/sample_processor/src/ifu.sv
+++ examples/exercises/lynn/sample_processor/src/ifu.sv
@@ -1,6 +1,6 @@
 // riscvsingle.sv
 // RISC-V single-cycle processor
-// David_Harris@hmc.edu 2020 kacassidy@hmc.edu 2025
+// pclark@hmc.edu mconine@hmc.edu 2026
 
 module ifu(
         // Inputs
@@ -15,24 +15,25 @@ module ifu(
         output  logic [31:0]    InstrD, PCD, PCPlus4D,
 
         // Memory interface   
-        output  logic           PCF,
-        input   logic           InstrF,        
+        output  logic [31:0]    PCF,
+        input   logic [31:0]    InstrF       
     );
 
-    logic   [31:0]  PCNext, PCF, PCPlus4F, InstrF;
+    logic   [31:0]  PCNext, PCPlus4F;
     
-    // TODO: make sure this mux is in the right order
+    // PC mux
     mux2 #(32) pcmux(PCPlus4F, PCTargetE, PCSrcE, PCNext);
 
     // Pipeline Register F-Stage
-    flopen PCFReg(clk, ~StallF, PCNext, PCF);
+    flopen #(32) PCFReg(clk, ~StallF, PCNext, PCF);
 
+    // PC+4 adder
     adder PCadd4f(PCF, 32'd4, PCPlus4F);
     
     // Pipeline Register D-Stage
-    flopenrc #(32) RD1EReg(clk, reset, FlushD, ~StallD, InstrF, InstrD);
-    flopenrc #(32) RD1EReg(clk, reset, FlushD, ~StallD, PCF, PCD);
-    flopenrc #(32) RD1EReg(clk, reset, FlushD, ~StallD, PCPlus4F, PCPlus4D);
+    flopenrc #(32) InstrDReg  (clk, reset, FlushD, ~StallD, InstrF,    InstrD);
+    flopenrc #(32) PCDReg     (clk, reset, FlushD, ~StallD, PCF,       PCD);
+    flopenrc #(32) PCPlus4DReg(clk, reset, FlushD, ~StallD, PCPlus4F,  PCPlus4D);
 
 endmodule
 
